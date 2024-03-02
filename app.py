c13ef1d8f19ad402ae679ef43e9073ad8124bf59
mwael2293 <mwael2281993@gmail.com>
2024-03-03 01:16:36 +0200

    adding status filed Temperature average value

--- app.py
+++ app.py
@@ -1,47 +1,48 @@
 from datetime import datetime, timedelta
-from flask import Flask
+from flask import Flask, jsonify
 import requests
 
 app = Flask(__name__)
 
 APP_VERSION = "0.0.1"
-
 TARGET_ID = "5eba5fbad46fb8001b799786"
 
 @app.route("/")
 def index():
-
     return __name__
 
 @app.route("/version")
 def version():
-
     return APP_VERSION
 
 @app.route("/temperature")
 def temperature():
-
-    r= requests.get(f"https://api.opensensemap.org/boxes/{TARGET_ID}?format=json",timeout=600)
+    r = requests.get(f"https://api.opensensemap.org/boxes/{TARGET_ID}?format=json", timeout=600)
     data = r.json()
     temps = data["sensors"]
     avg_temperature = ""
     time_of_request = ""
 
-    for temp in temps :
-        if temp ["title"]  == "Temperatur" :
+    for temp in temps:
+        if temp["title"] == "Temperatur":
             avg_temperature = temp['lastMeasurement']['value']
             time_of_request = temp['lastMeasurement']['createdAt']
-    lasttime_datatime = datetime.strptime(time_of_request,"%Y-%m-%dT%H:%M:%S.%fZ")
+    lasttime_datatime = datetime.strptime(time_of_request, "%Y-%m-%dT%H:%M:%S.%fZ")
     timenow = datetime.now()
-    #print (timenow - lasttime_datatime) used to troubleshoot to print temprature
 
-    if timenow - lasttime_datatime <= timedelta(hours=1) :
+    if timenow - lasttime_datatime > timedelta(hours=1):
+        return "Data could not be returned as it exceeds 1 hour"
 
-        return avg_temperature
+    status = get_temperature_status(float(avg_temperature))
+    return jsonify({"temperature": avg_temperature, "status": status})
 
-    return "data couldnot be returned as it exceed 1 hour "
-
+def get_temperature_status(temperature):
+    if temperature < 10:
+        return "Too Cold"
+    elif 11 <= temperature <= 36:
+        return "Good"
+    else:
+        return "Too Hot"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
- 
